6fd13d61a3d0f460197845c68cf1668494f47cac
unknown <kevinsuviacruz@gmail.com>
2024-07-22 21:05:48 -0300

    cambiando config

--- back-end/config.js
+++ back-end/config.js
@@ -1,3 +1,9 @@
+import 'dotenv/config'
 export const {
-  PORT = 3000
+  PORT = 3000,
+  HOST_DATABASE,
+  USER_DATABASE,
+  PORT_DATABASE,
+  PASSWORD_DATABASE,
+  NAME_DATABASE
 } = process.env
--- back-end/database/databaseConfig.js
+++ back-end/database/databaseConfig.js
@@ -1,13 +1,16 @@
 import mysql from 'mysql2/promise'
-
+import {
+  HOST_DATABASE, USER_DATABASE, PORT_DATABASE,
+  PASSWORD_DATABASE, NAME_DATABASE
+} from '../config.js'
+console.log(process.env.HOST_DATABASE)
 let connection
-
 const config = {
-  host: 'localhost',
-  user: 'root',
-  port: '3306',
-  password: '123456',
-  database: 'sweet_mint_db'
+  host: HOST_DATABASE,
+  user: USER_DATABASE,
+  port: PORT_DATABASE,
+  password: PASSWORD_DATABASE,
+  database: NAME_DATABASE
 }
 
 try {
